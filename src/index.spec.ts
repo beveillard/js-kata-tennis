a78f54171d5f39c0f6dc89cce7253484beb0d1b7
Benoît Eveillard <benoit.eveillard@gmail.com>
2020-12-28 17:16:34 -0500

    feature(tennis) implement

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,85 @@
-import { systemUnderTest } from ".";
+import { tennis, extractPoints1, extractPoints2, incrementPoints } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of tennis()", function () {
+
+  it("Should return 15", function () {
+    expect(extractPoints1("15 - 30")).toEqual("15");
   });
+
+  it("Should return 30", function () {
+    expect(extractPoints2("15 - 30")).toEqual("30");
+  });
+
+  it("Should return 40", function () {
+    expect(incrementPoints("30")).toEqual("40");
+  });
+
+  it("Should return +", function () {
+    expect(incrementPoints("40")).toEqual("+");
+  });
+
+  it("Should return *", function () {
+    expect(incrementPoints("+")).toEqual("*");
+  });
+
+  it("Should return '15 - 0'", function () {
+    expect(tennis("0 - 0", 1)).toEqual("15 - 0");
+  });
+
+  it("Should return '0 - 15'", function () {
+    expect(tennis("0 - 0", 2)).toEqual("0 - 15");
+  });
+
+  it("Should return '40 - 30'", function () {
+    expect(tennis("30 - 30", 1)).toEqual("40 - 30");
+  });
+
+  it("Should return '30 - 40'", function () {
+    expect(tennis("30 - 30", 2)).toEqual("30 - 40");
+  });
+
+  it("Should return 'Deuce'", function () {
+    expect(tennis("30 - 40", 1)).toEqual("Deuce");
+  });
+
+  it("Should return 'Deuce'", function () {
+    expect(tennis("40 - 30", 2)).toEqual("Deuce");
+  });
+
+  it("Should return 'Deuce'", function () {
+    expect(tennis("Advantage player 1", 2)).toEqual("Deuce");
+  });
+
+  it("Should return 'Deuce'", function () {
+    expect(tennis("Advantage player 2", 1)).toEqual("Deuce");
+  });
+
+  it("Should return 'Advantage player 1'", function () {
+    expect(tennis("Deuce", 1)).toEqual("Advantage player 1");
+  });
+
+  it("Should return 'Advantage player 2'", function () {
+    expect(tennis("Deuce", 2)).toEqual("Advantage player 2");
+  });
+
+  it("Should return 'Player 1 wins the game'", function () {
+    expect(tennis("40 - 30", 1)).toEqual("Player 1 wins the game");
+  });
+
+  it("Should return 'Player 1 wins the game'", function () {
+    expect(tennis("Advantage player 1", 1)).toEqual("Player 1 wins the game");
+  });
+
+  it("Should return 'Player 2 wins the game'", function () {
+    expect(tennis("30 - 40", 2)).toEqual("Player 2 wins the game");
+  });
+
+  it("Should return 'Player 2 wins the game'", function () {
+    expect(tennis("Advantage player 2", 2)).toEqual("Player 2 wins the game");
+  });
+
+  it("Should return '*'", function () {
+    expect(tennis("Advantage player 2", 3)).toEqual("*");
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,88 @@
-export function systemUnderTest(): boolean {
-  return true;
+const deuce = "Deuce";
+const advantage1 = "Advantage player 1";
+const advantage2 = "Advantage player 2";
+const winner1 = "Player 1 wins the game";
+const winner2 = "Player 2 wins the game";
+const separator = " - ";
+const points40 = "40";
+const pointsPlus = "+";
+const pointsList = ["0", "15", "30", points40, pointsPlus];
+const error = "*";
+
+export function tennis(score, winner) {
+
+  switch (winner) {
+    case 1:
+      return player1wins(score);
+    case 2:
+      return player2wins(score);
+    default:
+      return error;
+  }
+
+}
+
+export function extractPoints1(score) {
+  let index = score.indexOf(separator);
+
+  return score.slice(0, index);
+}
+
+export function extractPoints2(score) {
+  let index = score.indexOf(separator);
+
+  return score.slice(index + 3);
+}
+
+export function incrementPoints(points) {
+
+  for (let index = 0; index < (pointsList.length - 1); index++) {
+    if (points === pointsList[index]) {
+      return pointsList[++index];
+    }
+  }
+
+  return error;
+}
+
+function player1wins(score) {
+
+  switch (score) {
+    case deuce:
+      return advantage1;
+    case advantage1:
+      return winner1;
+    case advantage2:
+      return deuce;
+    default:
+      let points1 = incrementPoints(extractPoints1(score));
+      let points2 = extractPoints2(score);
+      return display(points1, points2);
+  }
+
+}
+
+function player2wins(score) {
+
+  switch (score) {
+    case deuce:
+      return advantage2;
+    case advantage1:
+      return deuce;
+    case advantage2:
+      return winner2;
+    default:
+      let points1 = extractPoints1(score);
+      let points2 = incrementPoints(extractPoints2(score));
+      return display(points1, points2);
+  }
+
+}
+
+function display(points1, points2) {
+  if ((points1 == points40) && (points2 == points40)) return deuce;
+  if (points1 == pointsPlus) return winner1;
+  if (points2 == pointsPlus) return winner2;
+
+  return points1 + separator + points2;
 }
