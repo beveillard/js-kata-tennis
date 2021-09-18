599f7009c83e997777b7816faf2dc9a76aec5d91
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-09-18 09:14:51 -0400

    Tennis

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-# js-kata-starter
+# Tennis
 
-Starter project for kata (Node, TypeScript, Jest)
+A tennis match is composed of points, games and sets. The rules of counting points within a game are exposed next. Your task is to implement this logic and expose a method which returns the current score as a string for display.
 
-## Installation
+- Each player can have either of these points in one game: 0, 15, 30 and 40.
+- If you have 40 and you win the ball, you win the game.
+- However:
+  - If both have 40 the players are "deuce".
+  - If the game is deuce, the winner of a ball will have "advantage".
+  - If the player with advantage wins the ball he wins the game.
+  - If the player without advantage wins they are back at deuce.
 
-```text
-git clone https://github.com/mathieueveillard/js-kata-starter.git
-cd js-kata-starter
-npm install
-```
-
-## Development
-
-```text
+# Run
 npm test
-```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
-export function systemUnderTest(): boolean {
-  return true;
+export enum WINNER {
+  PLAYER_1,
+  PLAYER_2,
+}
+
+export const MESSAGE = {
+  DEUCE: "Deuce",
+  ADVANTAGE_PLAYER_1: "Advantage player 1",
+  ADVANTAGE_PLAYER_2: "Advantage player 2",
+  PLAYER_1_WINS: "Player 1 wins",
+  PLAYER_2_WINS: "Player 2 wins",
+  ERROR: "Error",
+}
+
+const SEPARATOR = " - ";
+const BASIC_SCORES = ["0", "15", "30", "40"];
+
+export class Score {
+  scorePlayer1: number;
+  scorePlayer2: number;
+}
+
+export function tennis({ scorePlayer1: p1, scorePlayer2: p2 }: Score, winner: WINNER): Score {
+
+  return (winner === WINNER.PLAYER_1) ? { scorePlayer1: p1 + 1, scorePlayer2: p2 } : { scorePlayer1: p1, scorePlayer2: p2 + 1 };
+
+}
+
+export function displayScore({ scorePlayer1: p1, scorePlayer2: p2 }: Score): string {
+
+  if (p1 < 3) {
+    if (p2 > 3) return MESSAGE.PLAYER_2_WINS;
+    return displayBasicScore({ scorePlayer1: p1, scorePlayer2: p2 });
+  }
+  if (p2 < 3) {
+    if (p1 > 3) return MESSAGE.PLAYER_1_WINS;
+    return displayBasicScore({ scorePlayer1: p1, scorePlayer2: p2 });
+  }
+  if (p1 === p2) return MESSAGE.DEUCE;
+  if (p1 === p2 + 1) return MESSAGE.ADVANTAGE_PLAYER_1;
+  if (p2 === p1 + 1) return MESSAGE.ADVANTAGE_PLAYER_2;
+  if (p1 === p2 + 2) return MESSAGE.PLAYER_1_WINS;
+  if (p2 === p1 + 2) return MESSAGE.PLAYER_2_WINS;
+  return MESSAGE.ERROR;
+}
+
+export function displayBasicScore({ scorePlayer1: p1, scorePlayer2: p2 }: Score) {
+
+  return BASIC_SCORES[p1] + SEPARATOR + BASIC_SCORES[p2];
+
 }
